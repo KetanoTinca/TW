fbfd8ff30b33d4bd3046ed1ce5184cda55e5a286
andreibirleanu <andreicristianbirleanu@gmail.com>
2019-04-27 17:08:28 +0300

    login and session first day

---
+++ views/search.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="../CSS/search.css">
+    <script src="main.js"></script>
+</head>
+
+<body>
+    <div class="ui">
+    <?php include 'navbar.php';?>
+        <div class="s003">
+            <form>
+              <div class="inner-form">
+               
+                <div class="input-field second-wrap">
+                  <input id="search" type="text" placeholder="Enter Keywords?" />
+                </div>
+                <div class="input-field third-wrap">
+                  <button class="btn-search" type="button">
+                    Search
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+    </div>
+
+</body>
+
+</html>
---
+++ views/user.description.php
@@ -0,0 +1,30 @@
+<section id ="description">
+            <div class="containerdescription" >
+                <div class="container">
+                    <button class="profile_shape" onclick="location.href='change_photo.php'">
+                        <img  class="profile_pic" src="../img/profile.png"  alt="profile">
+                        <p class="change_profile">Change</p>
+                    </button>
+                    <div class="container">
+                        <div class="username">
+                            <h1>NumeProfesorPrenumeProfesor</h1>
+                            <p >nume.prenume@info.uaic.ro</p>
+                        </div>
+                        <button class="edit_profile" onclick="location.href='settings.php'">
+                                <img  class="edit_img" src="../img/edit.png" alt="Edit" >
+                                <p style="float:right;margin:5px 5px 0 0;">Edit profile</p>
+                        </button>
+                    </div>
+                </div>
+                </div>
+            
+            <div class="container">
+                <nav>
+                    <ul>
+                        <li  style="background:white;"><a href="user.php">Timeline</a></li>
+                        <li><a href="cards.php">View Students</a></li>
+                        <li><a href="settings.php">Settings</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
