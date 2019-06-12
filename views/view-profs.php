9776143047928a7b0d1b9cd2f03ca779d68a3479
Onatek <ketano1711@gmail.com>
2019-06-12 18:54:07 +0300

    fixed up

--- views/view-profs.php
+++ views/view-profs.php
@@ -15,82 +15,27 @@
 
     <div id="surface">
 
-    <?php
-    function redirect($url, $statusCode = 303)
-    {
-        header('Location: ' . $url, true, $statusCode);
-        die();
-    }
+        <?php
+        function redirect($url, $statusCode = 303)
+        {
+            header('Location: ' . $url, true, $statusCode);
+            die();
+        }
 
-    include 'navbar.php';
-    include '../classes/Database.php';
-    include '../controller/session.php';
-    if(isset($_SESSION['userType']) && $_SESSION['userType']=='student'){
+        include 'navbar.php';
+        include '../classes/Database.php';
+        include '../controller/session.php';
+        if (isset($_SESSION['userType']) && $_SESSION['userType'] == 'student') {
 
-        include 'choose_prof.php';
-    }else{
-
-    }
+            include 'choose_prof.php';
+        } else { }
 
 
-    ?>
+        ?>
 
 
 
-                ?>
-            <div id="description">
-                <h1>Pick the teacher that best suits your purpose!</h1>
-                <p>Here you have the name of the teacher, along with the subjects they cover in their work. If you like
-                    what
-                    you see, you can sign-up and complete the form, to check if you fit the description of their ideal
-                    student.</p>
-            </div>
-            <table id="teachers">
-                <tr>
-                    <th>Teachers</th>
-                    <th>Subjects</th>
-                    <th></th>
-                </tr>
-                <tr>
-                    <th>Name, Surname</th>
-                    <th><a href="./form.html">Subject1</a></th>
-                    <th><button class="small_button"
-                            onclick="location.href='./application-form.php'">Apply</button>
-                    </th>
-                </tr>
-                <tr>
-                    <th>Name, Surname</th>
-                    <th><a href="./form.html">Subject2</a></th>
-                    <th><button class="small_button"
-                            onclick="location.href='./application-form.php'">Apply</button>
-                    </th>
-                </tr>
-                <tr>
-                    <th>Name, Surname</th>
-                    <th><a href="./teacher-descriptions/name-surname">Subject3</a></th>
-                    <th><button class="small_button"
-                            onclick="location.href='./application-form.php'">Apply</button>
-                    </th>
-                </tr>
-                <tr>
-                    <th>Name, Surname</th>
-                    <th><a href="./form.html">Subject4</a></th>
-                    <th><button class="small_button"
-                            onclick="location.href='./application-form.php'">Apply</button>
-                    </th>
-                </tr>
-                <tr>
-                    <th>Name, Surname</th>
-                    <th><a href="./form.html">Subject5</a></th>
-                    <th><button class="small_button"
-                            onclick="location.href='./application-form.php'">Apply</button>
-                    </th>
-                </tr>
-            </table>
-            <div class="button_wrapper">
-                <button class="small_button" id="cancel_button">Cancel your current thesis request</button>
-            <div>
-        </section>
+
         <footer>
             <p>AcaTisM App, Copyright &copy; 2019</p>
         </footer>
